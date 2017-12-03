4d07d1b6682f443e19325f9ed81ea54f5059a274
Peter Rosellen <prosellen@gmx.net>
2017-12-03 16:06:53 +0100

    Vernünftigen JS-Code durch hässliches jQuery ersetzt

--- js/main.js
+++ js/main.js
@@ -3,81 +3,36 @@
     var data = {};
     var form;
 
-
-
-    function foo2()
-    {
-        return
-        {
-            bar: "hello"
-        };
-    }
-
-    var over18 = function(element) {
-       // return !element.data.over_18;
-        return true;
-    };
-
-    var nosticky = function(element) {
-        return !element.data.stickied;
-    };
-
-    // function success(data) {
-    //     console.table(data.data.children);
-    // }
-    //
-    // // "sticky" entfernen
-    //
-    // $.ajax({
-    //     url: 'https://www.reddit.com/r/javascript/hot.json',
-    //     data: { 'limit': 5 },
-    //     success: success,
-    //     dataType: 'json'
-    // });
-
     var buildList = function (entries) {
-        var container = document.querySelector('#container');
-        var oldEntries = container.querySelectorAll('.entry');
-
-        oldEntries.forEach(function(entry) {
-            container.removeChild(entry);
-        });
-
-        entries.forEach(function (t) {
+        $.each(entries, function (k,t) {
             if(t.data.thumbnail == '' || t.data.thumbnail == 'self' ) return;
 
-            var entry = document.createElement('a');
-            entry.classList.add('entry');
-            entry.href = t.data.url;
-            entry.target = "_blank";
+            var entry = $("<a />", {
+                class: 'entry',
+                href: t.data.url,
+                target: '_blank'
+            }).data('score', t.data.score).data('score', t.data.score).data('ups', t.data.ups).data('created', t.data.created);
 
-            var figure = document.createElement('figure');
-            figure.href = t.data.url;
-            figure.target = "_blank";
+            var figure = $("<figure />");
+            $("<img />", {
+                    src: t.data.thumbnail
+            }).appendTo(figure);
+            $("<figcaption />").html(t.data.title).appendTo(figure);
 
-            var thumbnail = document.createElement('img');
-            thumbnail.src = t.data.thumbnail;
-            figure.appendChild(thumbnail);
-
-            var title = document.createElement('figcaption');
-            title.innerHTML = t.data.title;
-            figure.appendChild(title);
-
-            entry.appendChild(figure);
-            container.appendChild(entry);
+            entry.append(figure);
+            $('#container').append(entry);
         });
 
     };
 
     var handleOnLoad = function (res) {
-        var entries = res.data.children.filter(nosticky).filter(over18);
+        var entries = res.data.children;
 
         buildList(entries);
     };
 
     var getJSON = function(sub, cat, limit) {
         var url = 'https://www.reddit.com/r/'+sub+'/'+cat+'.json';
-        var httpRequest = new XMLHttpRequest();
 
         $.ajax({
             url: url,
@@ -88,10 +43,6 @@
             success: handleOnLoad,
             dataType: 'json'
         });
-        // httpRequest.onload = handleOnLoad;
-        // httpRequest.responseType = 'json';
-        // httpRequest.open('GET', url, true);
-        // httpRequest.send({ limit: limit });
     };
 
     var init = function() {
@@ -116,18 +67,3 @@
 
 
 })(window, jQuery);
-
-
-/**
- /r/javascript
- /r/node/
- /r/aww/
-
-
-
- Erweiterung: NSFW ausschließen
- Nach Score sortieren
-
-
- Im Firefox wird die Liste nicht gelöscht, wenn man NSFW-Subreddits aufruft, im Chrome schon
-*/
